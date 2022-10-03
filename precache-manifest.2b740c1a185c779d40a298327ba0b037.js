386a8bb366490f4edd8a61021a2c46b7176a6a8c
Day shift <sharad.pathak@nbgspl.com>
2022-10-03 16:54:29 -0400

    first commit

---
+++ precache-manifest.2b740c1a185c779d40a298327ba0b037.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e9092c71f3282046d643aaf3a3f326",
+    "url": "/index.html"
+  },
+  {
+    "revision": "172277be694a9df87cff",
+    "url": "/static/css/2.d17711cb.chunk.css"
+  },
+  {
+    "revision": "af0e21eecbcf14be98d7",
+    "url": "/static/css/3.465e0326.chunk.css"
+  },
+  {
+    "revision": "9a3ce01edfee571df07c",
+    "url": "/static/css/4.1c9ef103.chunk.css"
+  },
+  {
+    "revision": "d2e98b9ae3391c8bbc98",
+    "url": "/static/css/5.e844406e.chunk.css"
+  },
+  {
+    "revision": "165923631a162ac33005",
+    "url": "/static/css/6.98fb6480.chunk.css"
+  },
+  {
+    "revision": "7c51777dce9c98ad75fa",
+    "url": "/static/css/7.2b8b1db5.chunk.css"
+  },
+  {
+    "revision": "5ae6e382a5acf8dba821",
+    "url": "/static/css/main.b82af128.chunk.css"
+  },
+  {
+    "revision": "172277be694a9df87cff",
+    "url": "/static/js/2.b5ba2753.chunk.js"
+  },
+  {
+    "revision": "cd830c1448fca4aa17173ea84f8a1908",
+    "url": "/static/js/2.b5ba2753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af0e21eecbcf14be98d7",
+    "url": "/static/js/3.b189875f.chunk.js"
+  },
+  {
+    "revision": "43f12eb89fac0494dbaa83a5bad043a1",
+    "url": "/static/js/3.b189875f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a3ce01edfee571df07c",
+    "url": "/static/js/4.6eb2a12e.chunk.js"
+  },
+  {
+    "revision": "d2e98b9ae3391c8bbc98",
+    "url": "/static/js/5.9350e94c.chunk.js"
+  },
+  {
+    "revision": "165923631a162ac33005",
+    "url": "/static/js/6.210f9a90.chunk.js"
+  },
+  {
+    "revision": "7c51777dce9c98ad75fa",
+    "url": "/static/js/7.43fb03ca.chunk.js"
+  },
+  {
+    "revision": "500393732b7e0a4b0630",
+    "url": "/static/js/8.a32d3678.chunk.js"
+  },
+  {
+    "revision": "5ae6e382a5acf8dba821",
+    "url": "/static/js/main.76b564c5.chunk.js"
+  },
+  {
+    "revision": "0bfe5258268f3f75accf",
+    "url": "/static/js/runtime-main.95e34887.js"
+  },
+  {
+    "revision": "5f9cff90925eaa3cf881723ad6cecad4",
+    "url": "/static/media/Shopcart.5f9cff90.png"
+  },
+  {
+    "revision": "4f6f969441253dff89714466bd9ad3ff",
+    "url": "/static/media/bgdemo1.4f6f9694.jpg"
+  },
+  {
+    "revision": "b82021c4a30741778afe34734ab5d83d",
+    "url": "/static/media/loader.b82021c4.gif"
+  },
+  {
+    "revision": "ee2b325bb5769f7bd06905cebfca0b6c",
+    "url": "/static/media/logo.ee2b325b.png"
+  },
+  {
+    "revision": "ee2b325bb5769f7bd06905cebfca0b6c",
+    "url": "/static/media/logo3.ee2b325b.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "4920ca36efd01712741f5a6154970eb3",
+    "url": "/static/media/user.4920ca36.png"
+  }
+]);
